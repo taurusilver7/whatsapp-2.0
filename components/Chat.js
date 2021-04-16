92a18477ea0b9080881d6f6e1136264fda01f074
taurusilver7 <prasad20697@gmail.com>
2021-04-16 23:29:20 +0530

    chat component linked to db

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,34 @@
 import { Avatar } from "@material-ui/core";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 
+import getRecipientEmail from "../utils/getRecipientEmail";
+import { auth, db } from "../firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { useRouter } from "next/router";
+
 const Chat = ({ id, users }) => {
+  const router = useRouter();
+
+  const [user] = useAuthState(auth);
+  const [recipientSnapshot] = useCollection(
+    db.collection("users").where("email", "==", getRecipientEmail(users, user))
+  );
+
+  const enterChat = () => {
+    router.push(`/chat/${id}`);
+  };
+  const recipient = recipientSnapshot?.docs?.[0]?.data();
+  const recipientEmail = getRecipientEmail(users, user);
+
   return (
-    <Container>
-      <UserAvatar />
-      <p>Recipient email</p>
+    <Container onClick={enterChat}>
+      {recipient ? (
+        <UserAvatar src={recipient?.photoURL} />
+      ) : (
+        <UserAvatar src={recipientEmail[0]}></UserAvatar>
+      )}
+      <p>{recipientEmail}</p>
     </Container>
   );
 };
@@ -14,7 +37,6 @@ export default Chat;
 
 const Container = styled.div`
   display: flex;
-  margin: 5px;
   align-items: center;
   cursor: pointer;
   padding: 15px;
