9139738026b0d2320f46471a40c693eacb02e45d
taurusilver7 <prasad20697@gmail.com>
2021-04-16 15:49:42 +0530

    components dir created

---
+++ components/Sidebar.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const Sidebar = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Sidebar
+
+const Container = styled.div`
+
+`;
