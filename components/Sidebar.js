7512b44e672af922ead60de6e899ea711ca1a4c7
taurusilver7 <prasad20697@gmail.com>
2021-04-16 20:33:50 +0530

    sidebar component completed

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,15 +1,119 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Chat from "./Chat";
+
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+
+import * as EmailValidator from "email-validator";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 const Sidebar = () => {
-    return (
-        <Container>
-            
-        </Container>
-    )
-}
+  const [user] = useAuthState(auth);
+  const userChatRef = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
+  const [chatSnapshot] = useCollection(userChatRef);
 
-export default Sidebar
+  const createChat = () => {
+    const input = prompt(
+      "Please enter an email addrs for the user wish to chat with!"
+    );
 
-const Container = styled.div`
+    if (!input) return null;
+    // an email-validator for valid user & a hook to avoid existing user added to chat.
+    if (
+      EmailValidator.validate(input) &&
+      !existChat(input) &&
+      input !== user.email
+    ) {
+      // add chat to database 'chats'
+      db.collection("chats").add({
+        users: [user.email, input],
+      });
+    }
+  };
+  const existChat = (recipientEmail) =>
+    // get a verification from real-time database('chats') about the users
+    // double bang > true for truthy value & false for null, undefined, empty string...
+    !!chatSnapshot?.docs.find(
+      (chat) =>
+        chat.data().users.find((user) => user === recipientEmail)?.length > 0
+    );
 
+  return (
+    <Container>
+      <Header>
+        <UserAvatar onClick={() => auth.signOut()} />
+        <IconContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
+
+      {/* chats */}
+      {chatSnapshot?.docs.map((chat) => (
+        <Chat key={chat.id} id={chat.id} users={chat.data().users} />
+      ))}
+    </Container>
+  );
+};
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconContainer = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+const SidebarButton = styled(Button)`
+  width: 100%;
+  && {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
 `;
