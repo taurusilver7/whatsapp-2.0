add234c3c71ecdb2a3a9cbd2e333481a161cdac0
taurusilver7 <prasad20697@gmail.com>
2021-04-16 11:22:53 +0530

    pages refactored for project

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div >
-      
+    <div>
+      <Head>
+        <title>Whatsapp 2.0</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
     </div>
   )
 }
