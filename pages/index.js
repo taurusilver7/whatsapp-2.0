efcbf64f63bc01a4f88f3e66b35d405ea2210dbe
taurusilver7 <prasad20697@gmail.com>
2021-04-16 15:48:52 +0530

    index refactored for project

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Whatsapp 2.0</title>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <h1>Whatsapp Build 2.0</h1>
+      <Sidebar />
     </div>
-  )
+  );
 }
