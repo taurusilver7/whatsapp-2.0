a925a6e49536d2c708d3944c85fa9ca2f2258c14
taurusilver7 <prasad20697@gmail.com>
2021-04-16 23:29:54 +0530

    chat screen on project created

---
+++ components/ChatScreen.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ChatScreen = () => {
+  return (
+    <Container>
+      <h1>This is the chat page</h1>
+    </Container>
+  );
+};
+
+export default ChatScreen;
+
+const Container = styled.div``;
