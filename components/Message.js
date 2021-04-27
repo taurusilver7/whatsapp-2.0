1f6b6008965ab0f372dd8ba6d831043b6261a93b
taurusilver7 <prasad20697@gmail.com>
2021-04-27 10:22:29 +0530

    chat message style initiated

--- components/Message.js
+++ components/Message.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
 
 const Message = ({ user, message }) => {
+  const [userLoggedIn] = useAuthState(auth);
   return (
     <Container>
       <p>{message.message}</p>
@@ -11,3 +14,14 @@ const Message = ({ user, message }) => {
 export default Message;
 
 const Container = styled.div``;
+
+const MessageElement = styled.p`
+  width: fit-content;
+  padding: 15px;
+  border-radius: 8px;
+  margin: 10px;
+  min-width: 60px;
+  padding-bottom: 26px;
+  position: relative;
+  text-align: right;
+`;
